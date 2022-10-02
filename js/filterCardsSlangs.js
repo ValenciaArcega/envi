f75a1c0f07e33206efd2b8c244e2d38c2ffcdb7a
Luis Angel Valencia Arcega <valencia.arcega@gmail.com>
2022-10-02 13:31:52 -0500

    slangs filter was added

---
+++ js/filterCardsSlangs.js
@@ -0,0 +1,19 @@
+document.addEventListener("keyup", e=> {
+
+    if (e.target.matches("#finder")) {
+  
+        if (e.key ==="Escape")e.target.value = ""
+        
+        document.querySelectorAll(".card-slang").forEach(carta => {
+            carta.textContent.toLowerCase().includes(e.target.value.toLowerCase())
+              ? carta.classList.remove("filter")
+              : carta.classList.add("filter")
+        })
+
+        document.querySelectorAll(".card-introduction").forEach(introCard => {
+            introCard.textContent.toLowerCase().includes(e.target.value.toLowerCase())
+                ? introCard.classList.remove("filter")
+                : introCard.classList.add("filter")
+        })
+    }
+  })
