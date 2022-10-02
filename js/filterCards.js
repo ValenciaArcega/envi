ef0490045a198ad24379bde40ac4ee44182e4adb
Luis Angel Valencia Arcega <valencia.arcega@gmail.com>
2022-10-02 11:40:06 -0500

    search bar was added

---
+++ js/filterCards.js
@@ -0,0 +1,15 @@
+document.addEventListener("keyup", e=> {
+
+    if (e.target.matches("#buscador")) {
+  
+        if (e.key ==="Escape")e.target.value = ""
+  
+        document.querySelectorAll(".card").forEach(carta => {
+  
+            carta.textContent.toLowerCase().includes(e.target.value.toLowerCase())
+              ? carta.classList.remove("filtro")
+              : carta.classList.add("filtro")
+        })
+  
+    }
+  })
