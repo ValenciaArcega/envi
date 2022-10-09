99fbf01144c367a7bba6772cebe79a6d60dc475d
Luis Angel Valencia Arcega <valencia.arcega@gmail.com>
2022-10-09 14:41:59 -0500

    text animation fade up, was added

---
+++ js/animation.js
@@ -0,0 +1,14 @@
+window.addEventListener('scroll', function() {
+
+    const text = document.querySelectorAll('.word-transition')[0];
+    const $height = window.innerHeight / 1.3;
+    const distance = text.getBoundingClientRect().top;
+
+    text.classList.add('fade_up');
+
+    if(distance <= $height) {
+        text.classList.add('aparece');
+    } else {
+        text.classList.remove('aparece');
+    }
+});
