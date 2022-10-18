c86400521bfd3e1f39547a587281301efdbe7c89
Luis Angel Valencia Arcega <valencia.arcega@gmail.com>
2022-10-18 17:14:59 -0500

    grid-auto-flow: dense and some filter was fixed

--- js/filterCards.js
+++ js/filterCards.js
@@ -15,6 +15,18 @@ document.addEventListener("keyup", e=> {
             introCard.textContent.toLowerCase().includes(e.target.value.toLowerCase())
                 ? introCard.classList.remove("filter")
                 : introCard.classList.add("filter")
+        }) 
+
+        document.querySelectorAll(".sliderTips").forEach(introCard => {
+            introCard.textContent.toLowerCase().includes(e.target.value.toLowerCase())
+                ? introCard.classList.remove("filter")
+                : introCard.classList.add("filter")
+        }) 
+
+        document.querySelectorAll(".card-slang").forEach(carta => {
+            carta.textContent.toLowerCase().includes(e.target.value.toLowerCase())
+              ? carta.classList.remove("filter")
+              : carta.classList.add("filter")
         })
     }
   })
