eb1d44ac1faa8a763f90c69d9c012937a3f77c43
Luis Angel Valencia Arcega <valencia.arcega@gmail.com>
2022-11-01 19:14:23 -0600

    features: new button to go to the top was added

--- js/top_bar.js
+++ js/top_bar.js
@@ -25,8 +25,18 @@ class TopBar extends HTMLElement {
                 <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18" />
                 </svg>
             </a>
+
+            <button class="btn-GoTop">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="icon-up">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 10.5L12 3m0 0l7.5 7.5M12 3v18" />
+                </svg>
+            </button>
         </div>`;
+
+        let btnUp = document.querySelector('.btn-GoTop');
+        btnUp.addEventListener('click', function(){
+          window.scrollTo({top: 0,left: 0,behavior: "smooth"});
+        });
     }
 }
-
 window.customElements.define("top-bar", TopBar);
