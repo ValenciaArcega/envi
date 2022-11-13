c7512545b5eebe96ded9e64c7e530592c875be4d
Luis Angel Valencia Arcega <valencia.arcega@gmail.com>
2022-11-13 09:01:25 -0600

    features: Darkmode and Lightmode was implemented

--- js/darkMode.js
+++
@@ -1,9 +0,0 @@
-let rootVariables = document.querySelector(':root');
-
-if (rootVariables) {
-  rootVariables.addEventListener('click', function setValor() {
-    rootVariables.style.setProperty('--white', '#181818');
-    rootVariables.style.setProperty('--grayColor', 'white');
-    rootVariables.style.setProperty('--grayBackground', '#121212');
-  });
-}
--- js/top_bar.js
+++ js/top_bar.js
@@ -26,6 +26,18 @@ class TopBar extends HTMLElement {
                 </svg>
             </a>
 
+            <button class="btn-Dark">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="icon-dark">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z" />
+                </svg>
+            </button>
+
+            <button class="btn-Light">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="icon-light">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z" />
+                </svg>
+            </button>
+
             <button class="btn-GoTop">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="icon-up">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 10.5L12 3m0 0l7.5 7.5M12 3v18" />
@@ -33,9 +45,32 @@ class TopBar extends HTMLElement {
             </button>
         </div>`;
 
+        let root = document.querySelector(':root');
         let btnUp = document.querySelector('.btn-GoTop');
-        btnUp.addEventListener('click', function(){
-          window.scrollTo({top: 0,left: 0,behavior: "smooth"});
+        let btnDark = document.querySelector('.btn-Dark');
+        let btnLight = document.querySelector('.btn-Light');
+        btnLight.classList.add('filter');
+
+        btnUp.addEventListener('click', function () {
+            window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+        });
+
+        btnDark.addEventListener('click', function() {
+            root.style.setProperty('--white', '#181818');
+            root.style.setProperty('--black', 'white');
+            root.style.setProperty('--grayBackground', '#121212');
+            root.style.setProperty('--iconTopBar','white');
+            btnDark.classList.add('filter');
+            btnLight.classList.remove('filter');
+        });
+
+        btnLight.addEventListener('click', function() {
+            root.style.setProperty('--white', '#fff');
+            root.style.setProperty('--black', '121212');
+            root.style.setProperty('--grayBackground', '#f2f2f7');
+            root.style.setProperty('--iconTopBar','black');
+            btnLight.classList.add('filter');
+            btnDark.classList.remove('filter');
         });
     }
 }
