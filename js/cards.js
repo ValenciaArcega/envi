7bc83fca9c8806140e2edb03a7e1cc909baedd0c
Luis Angel Valencia Arcega <valencia.arcega@gmail.com>
2022-11-13 12:50:49 -0600

    fixes: whole classes was moved to the card.js file, to reuse and peek better performance

--- js/cards.js
+++ js/cards.js
@@ -70,8 +70,7 @@ class CardElement extends HTMLElement {
         </ul>
       </div>`;
   }
-}
-
+} // class
 window.customElements.define("the-card", CardElement);
 
 
@@ -119,3 +118,84 @@ class CardSlang extends HTMLElement {
   }
 } // class
 window.customElements.define("slang-card", CardSlang);
+
+
+
+class CardIntroduction extends HTMLElement {
+
+  constructor() {
+    super();
+    this.card_title;
+    this.card_description;
+  }
+
+  static get observedAttributes() {
+    return ['title', 'description'];
+  }
+
+  attributeChangedCallback(Attr, oldValue, newValue) {
+    if (Attr == "title") {
+      this.card_title = newValue;
+    }
+    if (Attr == "description") {
+      this.card_description = newValue;
+    }
+  }
+
+  connectedCallback() {
+    this.innerHTML =
+      `<div class="card-introduction">
+          <div class="header-introduction">
+              <h1>${this.card_title}</h1>
+          </div>
+          <p>${this.card_description}</p>
+          <div class="card-introduction-container-icon">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
+                class="card-introduction-icon">
+                <path fill-rule="evenodd"
+                  d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z"
+                  clip-rule="evenodd" />
+              </svg>
+          </div>
+      </div>`;
+  }
+} // class
+window.customElements.define("intro-card", CardIntroduction);
+
+
+
+class CardTip extends HTMLElement {
+
+  constructor() {
+    super();
+    this.card_title;
+    this.card_tip_description;
+  }
+
+  static get observedAttributes() {
+    return ['card_title', 'tip_description'];
+  }
+
+  attributeChangedCallback(Attr, oldValue, newValue) {
+    if (Attr == "card_title") {
+      this.card_title = newValue;
+    }
+    if (Attr == "tip_description") {
+      this.card_tip_description = newValue;
+    }
+  }
+
+  connectedCallback() {
+    this.innerHTML =
+      `<div class="tip">
+            <div class="header-tip">
+            <h1>${this.card_title}</h1>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="icon-tip">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M12 18v-5.25m0 0a6.01 6.01 0 001.5-.189m-1.5.189a6.01 6.01 0 01-1.5-.189m3.75 7.478a12.06 12.06 0 01-4.5 0m3.75 2.383a14.406 14.406 0 01-3 0M14.25 18v-.192c0-.983.658-1.823 1.508-2.316a7.5 7.5 0 10-7.517 0c.85.493 1.509 1.333 1.509 2.316V18" />
+            </svg>            
+            </div>
+            <p>${this.card_tip_description}</p>
+         </div>`;
+  }
+} // class
+window.customElements.define("tip-card", CardTip);
