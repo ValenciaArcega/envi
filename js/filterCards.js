dc3249f0fd50785e4690c464b3d026b89eee3d50
Luis Angel Valencia Arcega <valencia.arcega@gmail.com>
2022-11-14 22:28:22 -0600

    fixes: icon appearance was fixed position to the center, also some bugs with the icon manifest for android devides

--- js/filterCards.js
+++ js/filterCards.js
@@ -1,4 +1,4 @@
-document.addEventListener("keyup", e=> {
+document.addEventListener("keyup", e => {
 
     if (e.target.matches("#finder")) {
   
@@ -8,31 +8,30 @@ document.addEventListener("keyup", e=> {
             carta.textContent.toLowerCase().includes(e.target.value.toLowerCase())
               ? carta.classList.remove("filter")
               : carta.classList.add("filter")
-        })
+        });
         
         document.querySelectorAll(".card-introduction").forEach(introCard => {
             introCard.textContent.toLowerCase().includes(e.target.value.toLowerCase())
                 ? introCard.classList.remove("filter")
                 : introCard.classList.add("filter")
-        }) 
+        });
 
         document.querySelectorAll(".card-slang").forEach(carta => {
             carta.textContent.toLowerCase().includes(e.target.value.toLowerCase())
               ? carta.classList.remove("filter")
               : carta.classList.add("filter")
-        })
+        });
 
         document.querySelectorAll('.sliderTips').forEach(carta => {
             carta.textContent.toLowerCase().includes(e.target.value.toLowerCase())
               ? carta.classList.remove("filter")
               : carta.classList.add("filter")
-        })
+        });
 
         document.querySelectorAll('.tip').forEach(carta => {
           carta.textContent.toLowerCase().includes(e.target.value.toLowerCase())
             ? carta.classList.remove("filter")
             : carta.classList.add("filter")
-      })
-
+      });
     }
-  })
+  });
--- js/topBar.js
+++ js/topBar.js
@@ -13,8 +13,7 @@ class TopBar extends HTMLElement {
     }
     connectedCallback() {
         this.innerHTML =
-`
-        <div class="top-bar">
+       `<div class="top-bar">
            <a href="${this.source_back}" class="btn-BackSections" aria-label="Go back">
                 <svg xmlns="http://www.w3.org/2000/svg"
                 fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="icon-back">
@@ -39,8 +38,8 @@ class TopBar extends HTMLElement {
                 <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
                 </svg>          
             </button>
-        </div>
-`;
+        </div>`;
+
         let root = document.querySelector(':root');
         let btnUp = document.querySelector('.btn-Search');
         let btnDark = document.querySelector('.btn-Dark');
