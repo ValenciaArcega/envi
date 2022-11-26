7ca79d7eba82f9c390a8c160b5df86f4c2ef865c
Luis Angel Valencia Arcega <valencia.arcega@gmail.com>
2022-11-26 12:44:02 -0600

    fixes: some measures was fixed

--- components/navBar/topBar.js
+++ components/navBar/topBar.js
@@ -14,7 +14,7 @@ class TopBar extends HTMLElement {
     connectedCallback() {
         this.innerHTML =
        `<div class="top-bar">
-           <a href="${this.source_back}" class="btn-BackSections" aria-label="Go back">
+           <a href="${this.source_back}" class="btn-BackHome" aria-label="Go back">
                 <svg xmlns="http://www.w3.org/2000/svg"
                 fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="icon-top-bar">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18" />
