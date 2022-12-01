462784f408422095308e472a9c5a02c620fd22b1
Luis Angel Valencia Arcega <valencia.arcega@gmail.com>
2022-11-30 20:54:10 -0600

    fixes: icon example was changed into card items

--- components/cards/card.js
+++ components/cards/card.js
@@ -57,10 +57,8 @@ class CardElement extends HTMLElement {
 
           <li>
             <div class="list-header">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                stroke="currentColor" class="card-icon">
-                <path stroke-linecap="round" stroke-linejoin="round"
-                  d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="card-icon">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M7.5 21L3 16.5m0 0L7.5 12M3 16.5h13.5m0-13.5L21 7.5m0 0L16.5 12M21 7.5H7.5" />
               </svg>
               <h4>Example</h4>
             </div>
