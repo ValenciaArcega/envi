395581e2ab4c8fe7f9e1c7d0991687d08f7d9b13
Luis Angel Valencia Arcega <valencia.arcega@gmail.com>
2023-02-03 21:02:30 -0600

    test: last arr test

--- test/tester.js
+++ test/tester.js
@@ -10,33 +10,33 @@ let output = '';
 
 for (let i = 0; i < tCards.length; i++) {
   output += ` 
-  <div class="card">
-      <h1 class="title-card">${tCards[i].cn}</h1>
-      <ul>
-        <li>
-          <div class="list-header">
-            <ion-icon class="card-svg" name="mic-outline"></ion-icon>
-            <h4>Pronunciation</h4>
-          </div>
-          <p>${tCards[i].pro}</p>
-        </li>
-
-        <li>
-          <div class="list-header">
-            <ion-icon class="card-svg" name="language-outline"></ion-icon>
-            <h4>Reverse context</h4>
-          </div>
-          <p>${tCards[i].es}</p>
-        </li>
-
-        <li>
-          <div class="list-header">
-            <ion-icon class="card-svg" name="extension-puzzle-outline"></ion-icon>             
-            <h4>Example</h4>
-          </div>
-          <p>${tCards[i].ex}</p>
-        </li>
-      </ul>
-  </div>`
+    <div class="card">
+        <h1 class="title-card">${tCards[i].cn}</h1>
+        <ul>
+          <li>
+            <div class="list-header">
+              <ion-icon class="card-svg" name="mic-outline"></ion-icon>
+              <h4>Pronunciation</h4>
+            </div>
+            <p>${tCards[i].pro}</p>
+          </li>
+  
+          <li>
+            <div class="list-header">
+              <ion-icon class="card-svg" name="language-outline"></ion-icon>
+              <h4>Reverse context</h4>
+            </div>
+            <p>${tCards[i].es}</p>
+          </li>
+  
+          <li>
+            <div class="list-header">
+              <ion-icon class="card-svg" name="extension-puzzle-outline"></ion-icon>             
+              <h4>Example</h4>
+            </div>
+            <p>${tCards[i].ex}</p>
+          </li>
+        </ul>
+    </div>`
 }
 document.querySelector('.cards').innerHTML = output;
