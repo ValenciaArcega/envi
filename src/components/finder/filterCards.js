c9ec6c71c4f4ce72b9b02851995654b42c18f43e
Luis Angel Valencia Arcega <valencia.arcega@gmail.com>
2023-02-08 10:42:53 -0600

    features: icon search was added in finder section

--- src/components/finder/filterCards.js
+++ src/components/finder/filterCards.js
@@ -1,31 +1,14 @@
+'use strict';
+
 document.addEventListener("keyup", e => {
-
   if (e.target.matches("#finder")) {
-
     if (e.key === "Escape") e.target.value = ""
 
     document.querySelectorAll('.card').forEach(carta => {
       carta.textContent.toLowerCase().includes(e.target.value.toLowerCase())
         ? carta.classList.remove("hidden")
         : carta.classList.add("hidden")
-    });
 
-    document.querySelectorAll(".card-introduction").forEach(introCard => {
-      introCard.textContent.toLowerCase().includes(e.target.value.toLowerCase())
-        ? introCard.classList.remove("hidden")
-        : introCard.classList.add("hidden")
-    });
-
-    document.querySelectorAll(".card-slang").forEach(carta => {
-      carta.textContent.toLowerCase().includes(e.target.value.toLowerCase())
-        ? carta.classList.remove("hidden")
-        : carta.classList.add("hidden")
-    });
-
-    document.querySelectorAll('.sliderTips').forEach(carta => {
-      carta.textContent.toLowerCase().includes(e.target.value.toLowerCase())
-        ? carta.classList.remove("hidden")
-        : carta.classList.add("hidden")
     });
 
     document.querySelectorAll('.tip').forEach(carta => {
